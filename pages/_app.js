0a659d99e7701c2bd41e457895eecb97db01ae96
Keesha P. Tiangson <mariokeesha@Keesha-MacBook-Air.local>
2026-01-26 04:14:10 -0700

    Day 2 Complete

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,3 @@ import '../styles/globals.css';
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
-useEffect(() => {
-  const setVh = () => {
-    document.documentElement.style.setProperty(
-      "--vh",
-      `${window.innerHeight * 0.01}px`
-    )
-  }
-  setVh()
-  window.addEventListener("resize", setVh)
-  return () => window.removeEventListener("resize", setVh)
-}, [])
